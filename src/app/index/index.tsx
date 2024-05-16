6560b95605e5dac5b940dece80788919eadb39bc
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2024-05-15 22:34:16 -0300

    4a aula

---
+++ src/app/index/index.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import {View, Text, ScrollView} from 'react-native';
+import { styles } from './styles';
+import { Ingredient } from '@/components/ingredient';
+import { Selected } from '@/components/selected';
+
+export default function Home(){
+    const [selected, setSelected] = useState<string[]>([])
+
+    function handleToggleSelected(value: string){
+        // console.log(value);
+        if(selected.includes(value)){
+            return setSelected( (state) => state.filter( (item) => item !== value) )
+        }
+        setSelected( (state) => [...state, value])
+        // console.log(selected);
+
+    }
+
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Escolha {"\n"}
+                <Text style={styles.subtitle}>os produtos</Text>
+            </Text>
+            <Text style={styles.message}>
+                Descuba receitas baseadas nos produtos que você escolheu
+            </Text>
+            {/* <Ingredient /> */}
+            <ScrollView 
+                contentContainerStyle={styles.ingredients}
+                showsVerticalScrollIndicator={false}
+            >
+                {Array.from( {length:100} ).map( (item, index) => (
+                    <Ingredient key={index}  
+                        name="Morango" image="" 
+                        selected={selected.includes(String(index))}
+                        onPress={() => handleToggleSelected(String(index))}
+                    />
+                ))}
+            </ScrollView>  
+            <Selected />          
+        </View>
+    )
+}
---
+++ src/components/ingredient/index.tsx
@@ -0,0 +1,27 @@
+import { Pressable, PressableProps, Text, Image } from 'react-native'
+import { styles } from './styles'
+
+export type ingredientsProps = {
+    name: string
+    image: string
+    selected?: boolean
+}
+
+export function Ingredient({
+    name, 
+    image,
+    selected = false,
+    ...rest
+}:ingredientsProps & PressableProps ){
+    return(
+        <Pressable style={[styles.container, selected && styles.selected ]}
+            {...rest}
+        >
+            <Image 
+                source={ require("@/assets/apple.png")} 
+                style={styles.image} 
+            />
+            <Text>{name}</Text>
+        </Pressable>
+    )
+}
