0d290ee6efd9972ea8e4eb60055caa67685d4bc1
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2024-09-16 19:54:15 -0300

    export default supabase

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,9 @@
 import "react-native-url-polyfill/auto"
-import { createClient } from "@supabase/supabase-js"
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? ""
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? ""
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase
