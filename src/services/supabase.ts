79bc896dc01e4e92e229e0a425be6fe8faeec6d0
FERNANDO JACINTO DA SILVA <fernando.jsilva9@senacsp.edu.br>
2024-09-16 21:03:28 -0300

    update variable enviroment config

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,9 +1,13 @@
-import "react-native-url-polyfill/auto"
+// import "react-native-url-polyfill/auto"
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? ""
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? ""
 
+// Verifica se as variáveis de ambiente estão definidas
+if (!supabaseUrl || !supabaseKey){
+    throw new Error("As variáveis de ambiente não foram configuradas corretamente!")
+}
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase
